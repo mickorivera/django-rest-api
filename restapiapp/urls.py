a52e1f6f424d9ac58b135da249cf987d91b13daa
Micko Rivera <magrivera.freelancer@gmail.com>
2017-10-01 22:25:12 +0800

    Change base url prefix from user to users

--- restapiapp/urls.py
+++ restapiapp/urls.py
@@ -3,21 +3,21 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import UserViewSet
 
 urlpatterns = format_suffix_patterns([
-    url(r'^user/$',
+    url(r'^users/$',
         UserViewSet.as_view({
                 'post': 'create',
                 'get': 'list'
             }),
         name='user-list'),
 
-    url(r'^user/(?P<pk>[0-9]+)/$',
+    url(r'^users/(?P<pk>[0-9]+)/$',
         UserViewSet.as_view({
                 'get': 'retrieve',
                 'patch': 'partial_update'
             }),
         name='user-detail'),
 
-    url(r'^user/(?P<pk>[0-9]+)/is_active/$',
+    url(r'^users/(?P<pk>[0-9]+)/is_active/$',
         UserViewSet.as_view({
                 'patch': 'activate'
             }),
